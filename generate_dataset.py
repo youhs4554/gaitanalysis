a3815485f07215261a896f36acc87b3543c7bc3c
hossay <youhs4554@gmail.com>
2019-08-05 14:52:07 +0900

    dataset geneartion add select gpu for yolo

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,7 +1,11 @@
 from utils.preprocessing import PatientLocalizer, COPAnalyizer, Worker
 import opts
+from preprocess.darknet.python.extract_bbox import set_gpu
 
 opt = opts.parse_opts()
+
+# patient localizer & interval selector
+set_gpu(opt.device_yolo)
 
 localizer = PatientLocalizer(darknet_api_home=opt.darknet_api_home)
 
